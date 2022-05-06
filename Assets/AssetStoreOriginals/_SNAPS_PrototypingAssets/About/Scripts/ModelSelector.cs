0b04e3afe1c27bc6c8a1099f09842c5262547a53
Anthony171717 <anthony.mack@telus.net>
2022-05-06 09:38:02 -0400

    Added custom text to correspond to each model

--- Assets/AssetStoreOriginals/_SNAPS_PrototypingAssets/About/Scripts/ModelSelector.cs
+++ Assets/AssetStoreOriginals/_SNAPS_PrototypingAssets/About/Scripts/ModelSelector.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 public class ModelSelector : MonoBehaviour
 {
     public GameObject[] models;
+    public GameObject[] texts;
     // Start is called before the first frame update
+    void Start(){
+        Destroy(GameObject.FindWithTag("Text"));
+        Instantiate(texts[0]);
+    }
 
     void Update(){
         //Destroy(GameObject.FindWithTag("Model"));
@@ -16,23 +21,33 @@ public class ModelSelector : MonoBehaviour
     public void HandleInputData(int val){
         if(val == 0){
             Destroy(GameObject.FindWithTag("Model"));
+            Destroy(GameObject.FindWithTag("Text"));
+            Instantiate(texts[0]);
         }
         if(val == 1){
             Destroy(GameObject.FindWithTag("Model"));
+            Destroy(GameObject.FindWithTag("Text"));
             Instantiate(models[0]);
+            Instantiate(texts[1]);
             
         }
         if(val == 2){
             Destroy(GameObject.FindWithTag("Model"));
+            Destroy(GameObject.FindWithTag("Text"));
             Instantiate(models[1]);
+            Instantiate(texts[2]);
         }
         if (val == 3){
             Destroy(GameObject.FindWithTag("Model"));
+            Destroy(GameObject.FindWithTag("Text"));
             Instantiate(models[2]);
+            Instantiate(texts[3]);
         }
         if(val == 4){
             Destroy(GameObject.FindWithTag("Model"));
+            Destroy(GameObject.FindWithTag("Text"));
             Instantiate(models[3]);
+            Instantiate(texts[4]);
         }
     }
 }
